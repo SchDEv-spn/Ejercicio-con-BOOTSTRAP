48e333227ff5c3c5398358fe0939e636648e9d81
schneider manrique <schneider.manrique@gmai.com>
2025-09-11 17:48:25 -0500

    empece con el responsive

--- js/main.js
+++ js/main.js
@@ -32,4 +32,40 @@ $(document).ready(function () {
             }
         ]
     })
+
+
+
+        $('.slider-reviews').slick({
+        slidesToShow: 3,         // cantidad de productos visibles
+        slidesToScroll: 1,
+        infinite: true,
+        arrows: true,
+        autoplay: true,
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 1024,     // tablets
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 600,     // móviles medianos
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 400,     // móviles pequeños
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    })
 });
