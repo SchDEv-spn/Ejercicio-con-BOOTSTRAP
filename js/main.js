e27302475aca85addbdf695a2dd30320e13b2b38
schneider manrique <schneider.manrique@gmai.com>
2025-09-13 22:07:57 -0500

    realice la section de reviews

--- js/main.js
+++ js/main.js
@@ -27,37 +27,30 @@ $('.product-slider').slick({
     }
   ]
 })
-        $('.slider-reviews').slick({
-        slidesToShow: 3,         // cantidad de productos visibles
-        slidesToScroll: 1,
-        infinite: true,
-        arrows: true,
-        autoplay: true,
-        dots: false,
-        responsive: [
-            {
-                breakpoint: 1024,     // tablets
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: false
-                }
-            },
-            {
-                breakpoint: 600,     // móviles medianos
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 400,     // móviles pequeños
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
+$('.slider-reviews').slick({
+    slidesToShow: 3,         // desktop
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: true,
+    autoplay: true,
+    dots: false,
+    responsive: [
+        {
+            breakpoint: 1024,     // tablets
+            settings: {
+                slidesToShow: 2,  // <-- aquí el cambio
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
             }
-        ]
-    })
+        },
+        {
+            breakpoint: 600,      // móviles medianos
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+})
 });
